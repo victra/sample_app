09ded0ebeaf058931a016f36c34ad813bed4665b
karir <karir@karir.com>
2016-02-25 17:41:46 +0700

    7.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,16 @@ Rails.application.routes.draw do
   get 'about'	=>	 'static_pages#about'
 
   get 'contact'	=>	 'static_pages#contact'
-  
+
   get 'signup'	=>	 'users#new'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
- 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
